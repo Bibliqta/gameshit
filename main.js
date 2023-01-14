560cd4e12ffd0fbb02846ca76c936823d4b54def
Bibliqta <102380250+Bibliqta@users.noreply.github.com>
2023-01-14 14:14:46 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,62 +1,84 @@
-const Game = new Phaser.Game(1350, 600, Phaser.AUTO, 'game-canvas', { preload, create,update })
+const Game = new Phaser.Game(1350, 600, Phaser.AUTO, 'game-canvas', { preload, create,update });
 
-let character, buttons, startbutton
+let character, buttons, startButton, backGround, platform
+let canMove = false;
 
 function preload() {
-    Game.load.image ('BackGround','Background.png')
-    Game.load.spritesheet ('Character','spritesheet.png',6400/4,1600)
-    Game.load.spritesheet ('Explosion','exp.png',3000/3,1000)
-    
+    Game.load.image ('BackGround','Background.png');
+    Game.load.spritesheet ('Character','spritesheet.png',6400/4,1600);
+    Game.load.spritesheet ('Explosion','exp.png',3000/3,1000);
+    Game.load.image('platform', 'rock.png');
 }
 
 function create(){
-    let background = Game.add.sprite(0,0,'BackGround')
-    Game.stage.backgroundColor = '#182d3b'
+    backGround = Game.add.sprite(0,0,'BackGround');
+    backGround.visible = false;
 
-    background .width=Game.width
-    background.height=Game.height
-    startbutton = Game.add.text(500, 500, 'Start', {fill: 'white', shadowColor: 'black'})
-    startbutton.setInteractive()
-    console.log(startbutton)
+    backGround .width=Game.width;
+    backGround.height=Game.height;
 
-     character = Game.add.sprite(0,500, 'Character')
-     
-    character.scale.set (0.03)
-    character.frame=1
+    startButton = Game.add.text(Game.width / 2, Game.height / 2, 'Start', { font: '24px Arial', fill: '#fff' });
+    startButton.anchor.set(0.5);
+    startButton.inputEnabled = true;
+    startButton.input.useHandCursor = true;
+    startButton.events.onInputDown.add(startGame, this);
+    startButton.events.onInputOver.add(onHover, this);
+    startButton.events.onInputOut.add(offHover, this);
 
-   buttons= Game.input.keyboard.createCursorKeys();
+    character = Game.add.sprite(0,500, 'Character');
+    character.scale.set (0.03);
+    character.frame=1;
+    Game.physics.arcade.enable(character);
+    character.body.collideWorldBounds = true;
+    character.body.bounce.y = 0.2;
+    character.body.gravity.y = 500;
+    character.body.linearDamping = 1;
+    character.body.drag.set(70);
 
-   KeyW = Game.input.keyboard.addKey(Phaser.KeyCode.W)
-   KeyA = Game.input.keyboard.addKey(Phaser.KeyCode.A)
-   KeyS = Game.input.keyboard.addKey(Phaser.KeyCode.S)
-   KeyD = Game.input.keyboard.addKey(Phaser.KeyCode.D)
+    platform = Game.add.sprite(0, 550, 'platform');
+    Game.physics.arcade.enable(platform);
+    platform.body.immovable = true;
+    platform.body.allowGravity = false;
+    
+
+    buttons= Game.input.keyboard.createCursorKeys();
+
+    KeyA = Game.input.keyboard.addKey(Phaser.KeyCode.A);
+    KeyD = Game.input.keyboard.addKey(Phaser.KeyCode.D);
+    SpaceKey = Game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+    ControlKey = Game.input.keyboard.addKey(Phaser.KeyCode.CONTROL);
 }
 
 function update() {
-    // Move with arrow keys 
-    if (buttons.right.isDown&&character.x<1325) {
-        character.x+=5; 
+    Game.physics.arcade.collide(character, platform);
+
+    if (!canMove) {
+        return;
     }
-    if (buttons.left.isDown&&character.x>0) {
-        character.x-=5;
+    // Character movement system 
+    if (KeyA.isDown) {
+        character.body.velocity.x = -150;
+    } else if (KeyD.isDown) {
+        character.body.velocity.x = 150;
+    } else {
+        character.body.velocity.x = 0;
     }
-    if (buttons.up.isDown&&character.y>0) {
-        character.y-=5; 
+
+    if (SpaceKey.isDown && character.body.onFloor()) {
+        character.body.velocity.y = -400;
     }
-    if (buttons.down.isDown&&character.y<550) {
-        character.y+=5; 
-    }
-    // Move with WASD
-    if (KeyD.isDown&&character.x<1325) {
-        character.x+=5; 
-    }
-    if (KeyA.isDown&&character.x>0) {
-        character.x-=5;
-    }
-    if (KeyW.isDown&&character.y>0) {
-        character.y-=5; 
-    }
-    if (KeyS.isDown&&character.y<550) {
-        character.y+=5; 
-    }
+}
+
+function onHover() {
+    startButton.fill = '#ff0000';
+}
+
+function offHover() {
+    startButton.fill = '#fff';
+}
+
+function startGame(){
+    backGround.visible = true;
+    canMove = true;
+    startButton.visible = false;
 }
