8ec8a59e2a73d21fe7e97fc1c261db9bae3543df
Bibliqta <102380250+Bibliqta@users.noreply.github.com>
2023-01-21 02:21:02 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,43 @@
 const Game = new Phaser.Game(1350, 600, Phaser.AUTO, 'game-canvas', { preload, create,update });
 
-let character, buttons, startButton, backGround, platform
+let character, buttons, startButton, backGround, platform, menuBackground, playButton, titleText;
 let canMove = false;
 
 function preload() {
     Game.load.image ('BackGround','Background.png');
     Game.load.spritesheet ('Character','spritesheet.png',6400/4,1600);
-    Game.load.spritesheet ('Explosion','exp.png',3000/3,1000);
     Game.load.image('platform', 'rock.png');
+    Game.load.image('menuBackground', 'menu_bg.png');
+    Game.load.spritesheet('playButton', 'play_button.png', 64, 64);
 }
 
 function create(){
+    menuBackground = Game.add.sprite(0,0,'menuBackground');
+    menuBackground.width = Game.width;
+    menuBackground.height = Game.height;
+
+    titleText = Game.add.text(Game.width / 2, 100, 'My Game', { font: '40px Arial', fill: '#fff' });
+    titleText.anchor.set(0.5);
+
+    playButton = Game.add.button(Game.width / 2, 300, 'playButton', startGame, this, 0, 0, 1);
+    playButton.anchor.set(0.5);
+    playButton.scale.set(2);
+
+    settingsButton = Game.add.button(Game.width - 50, 50, 'settingsButton', openSettings, this);
+    settingsButton.scale.set(0.5);
+
+    settingsFrame = Game.add.sprite(Game.width / 2, Game.height / 2, 'settingsFrame');
+    settingsFrame.anchor.set(0.5);
+    settingsFrame.visible = false;
+
     backGround = Game.add.sprite(0,0,'BackGround');
     backGround.visible = false;
-
-    backGround .width=Game.width;
-    backGround.height=Game.height;
-
-    startButton = Game.add.text(Game.width / 2, Game.height / 2, 'Start', { font: '24px Arial', fill: '#fff' });
-    startButton.anchor.set(0.5);
-    startButton.inputEnabled = true;
-    startButton.input.useHandCursor = true;
-    startButton.events.onInputDown.add(startGame, this);
-    startButton.events.onInputOver.add(onHover, this);
-    startButton.events.onInputOut.add(offHover, this);
+    backGround.width = Game.width;
+    backGround.height = Game.height;
 
     character = Game.add.sprite(0,500, 'Character');
     character.scale.set (0.03);
-    character.frame=1;
+    character.frame = 1;
     Game.physics.arcade.enable(character);
     character.body.collideWorldBounds = true;
     character.body.bounce.y = 0.2;
@@ -39,7 +49,6 @@ function create(){
     Game.physics.arcade.enable(platform);
     platform.body.immovable = true;
     platform.body.allowGravity = false;
-    
 
     buttons= Game.input.keyboard.createCursorKeys();
 
@@ -50,12 +59,11 @@ function create(){
 }
 
 function update() {
-    Game.physics.arcade.collide(character, platform);
-
     if (!canMove) {
         return;
     }
-    // Character movement system 
+    Game.physics.arcade.collide(character, platform);
+
     if (KeyA.isDown) {
         character.body.velocity.x = -150;
     } else if (KeyD.isDown) {
@@ -64,7 +72,8 @@ function update() {
         character.body.velocity.x = 0;
     }
 
-    if (SpaceKey.isDown && character.body.onFloor()) {
+    if
+ (SpaceKey.isDown && character.body.onFloor()) {
         character.body.velocity.y = -400;
     }
 }
@@ -77,8 +86,18 @@ function offHover() {
     startButton.fill = '#fff';
 }
 
-function startGame(){
+function startGame() {
+    menuBackground.visible = false;
+    titleText.visible = false;
+    playButton.visible = false;
     backGround.visible = true;
     canMove = true;
-    startButton.visible = false;
+}
+
+function openSettings() {
+    menuBackground.visible = false;
+    titleText.visible = false;
+    playButton.visible = false;
+    settingsButton.visible = false;
+    settingsFrame.visible = true;
 }
